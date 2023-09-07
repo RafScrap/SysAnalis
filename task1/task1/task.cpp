6957d7d375fde4c3358fd53a5d2606f4cbd56f03
Alexandra Bobrovskaya <black.sakyra@gmail.com>
2023-09-07 11:57:57 +0300

    task1

---
+++ task1/task1/task.cpp
@@ -0,0 +1,39 @@
+//В консоли, когда находимся в папке task1, идем в папку x64, а оттуда - в папку Debug
+//Как это сделать в Windows: cd ./x64/Debug
+//Запускаем task1.exe с именами файлов:
+//task1 inTable.csv inCoord.csv
+
+#include <fstream> 
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	std::ifstream fileInfo(argv[1]);
+	std::ifstream tableC(argv[2]);
+	std::string line, cell;
+	int x, y;
+	char delimiter = ';';
+	if (tableC.is_open()) {
+		getline(tableC, line); // Считываем строку
+		std::stringstream streamCountN(line); // Преобразование строки в поток
+		getline(streamCountN, cell, delimiter);
+		x = std::stoi(cell);
+		getline(streamCountN, cell, delimiter);
+		y = std::stoi(cell);
+		tableC.close();
+	}	
+
+	if (fileInfo.is_open()) {
+		for (int i = 0; i < x + 1; i++) {
+			getline(fileInfo, line); // Считываем строку
+		}
+		std::stringstream streamCountN(line); // Преобразование строки в поток
+		for (int i = 0; i < y + 1; i++) {
+			getline(streamCountN, cell, delimiter);
+		}
+		std::cout << cell;
+	}
+	fileInfo.close();
+}
