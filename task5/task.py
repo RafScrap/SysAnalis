e4ca78304ea346e1efbba5d360be0ffe27df7959
Alexandra Bobrovskaya <black.sakyra@gmail.com>
2024-01-16 00:09:45 +0300

    rewrited tasks 5 and 6

--- task5/task.py
+++ task5/task.py
@@ -1,59 +1,57 @@
 import numpy as np
 import json
 
-def flatListSize(data) -> int:
-    res = 0
-    for item in data:
-        if isinstance(item, list):
-            for l2 in item:
-                res += 1
-        else:
-            res += 1
-    return res 
+def makeMatrix(data: list) -> np.array:
+    setVisited = set()
+    M = list()
+    number = makeArrSize(data)
 
-def createRow(visited: set, cur: int, n:int) -> np.array:
+    for item in data:
+        if (type(item) != list):
+            setVisited.add(int(item))
+            row = makeRow(setVisited, number)
+            M.append({'num': int(item), 'row': row})
+        else:
+            for subItem in item:
+                setVisited.add(int(subItem))
+            for subItem in item:
+                row = makeRow(setVisited, number)
+                M.append({'num': int(subItem), 'row': row})
+
+    M.sort(key = (lambda x: x['num']))
+    rowResult = [item['row'] for item in M]
+
+    return np.array(rowResult)
+
+def makeRow(setVisited: set, number : int) -> np.array:
     row = []
-    for i in range(n):
-        row.append(1 if i+1 in visited else 0)
+    for k in range(number):
+        row.append(1 if k + 1 in setVisited else 0)
     return np.array(row)
 
-def createMatrix(data: list) -> np.array:
-    visited = set()
-    matrix = list()
-
-    n = flatListSize(data)
-
+def makeArrSize(data) -> int:
+    number = 0
     for elem in data:
-        if (type(elem) != list):
-            visited.add(int(elem))
-            row = createRow(visited=visited, cur=int(elem), n=n)
-            matrix.append({'num': int(elem), 'row': row})
+        if isinstance(elem, list):
+            number += len(elem)
         else:
-            for subelem in elem:
-                visited.add(int(subelem))
-            for subelem in elem:
-                row = createRow(visited=visited, cur=int(subelem), n=n)
-                matrix.append({'num': int(subelem), 'row': row})
+            number += 1
+    return number 
 
-    matrix.sort(key=(lambda x: x['num']))
-    raw = [elem['row'] for elem in matrix]
-
-    return np.array(raw)
-
-def isStrResultType(str1, str2) -> bool:
-    if (type(str1) == list) or (type(str2) == list):
+def isStringType(string1, string2) -> bool:
+    if (type(string1) == list) or (type(string2) == list):
         return True
 
-    str1 = json.loads(str1)
-    str2 = json.loads(str2)
+    string1 = json.loads(string1)
+    string2 = json.loads(string2)
     res = True
-    for elem in str1:
+    for elem in string1:
         if (type(elem) != list):
             res = res and (type(elem) == str)
         else:
             for subelem in elem:
                 res = res and (type(subelem) == str)
-    for elem in str2:
+    for elem in string2:
         if (type(elem) != list):
             res = res and (type(elem) == str)
         else:
@@ -61,66 +59,65 @@ def isStrResultType(str1, str2) -> bool:
                 res = res and (type(subelem) == str)
     return res
 
-def S(str1, str2, isStr) -> list:
-    str1 = eval(str1)
-    str2 = eval(str2)    
-    
-    matrix1 = createMatrix(str1)
-    matrix2 = createMatrix(str2)
-
-    matrix12 = matrix1 * matrix2
-    matrix12T = matrix1.T * matrix2.T
-
-    criterion = matrix12 | matrix12T 
-
-    answer = []
-    tmp = []
-    for i in range(criterion.shape[0]):
-        item = []
-        for j in range(criterion.shape[0]): #range(i):
-            if criterion[i][j] == 0:
-                if isStr:
-                    item.append(str(j+1))
-                else:
-                    item.append(j+1)
-
-        if len(item) > 0:
-            if isStr:
-                item.append(str(i+1))
-            else:
-                item.append(i+1)
-            tmp.append([item, 0])
-
-    for t in tmp:
-        if t[1] == 0:
-            r = t[0]
-            t[1] = 1
-            for l in tmp:
-                if l[1] == 0:
-                    m = list(set(r) & set(l[0]))
-                    if len(m) > 0:
-                        l[1] = 1
-                        r = list(set(r) | set(l[0]))
-            answer.append(r)
-
-    return answer
-
-def flatList(data) -> list:
+def makeArr(data) -> list:
     if (type(data) != list):
         data = json.loads(data)
-    res = []
+    arr = []
     i = 1
     for item in data:
         if isinstance(item, list):
             for l2 in item:
-                res.append((l2, i, 1, item))
+                arr.append((l2, i, 1, item))
                 i += 1
         else:
-            res.append((item, i, 0, []))
+            arr.append((item, i, 0, []))
             i += 1
-    return res 
+    return arr 
 
-def flatList2(data) -> list:
+def S(strA, strB, isStr) -> list:
+    strA = eval(strA)
+    M_A = makeMatrix(strA)
+    strB = eval(strB)    
+    M_B = makeMatrix(strB)
+
+    M_AB = M_A * M_B
+    M_AB_T = M_A.T * M_B.T
+    parameter = M_AB | M_AB_T 
+
+    result = []
+    temp = []
+    for i in range(parameter.shape[0]):
+        item = []
+        for k in range(parameter.shape[0]): #range(i):
+            if parameter[i][k] == 0:
+                if isStr:
+                    item.append(str(k + 1))
+                else:
+                    item.append(k + 1)
+
+        if len(item) > 0:
+            if isStr:
+                item.append(str(i + 1))
+            else:
+                item.append(i + 1)
+            temp.append([item, 0])
+
+    for arr in temp:
+        if arr[1] == 0:
+            res = arr[0]
+            arr[1] = 1
+            for arr2 in temp:
+                if arr2[1] == 0:
+                    m = list(set(res) & set(arr2[0]))
+                    if len(m) > 0:
+                        arr2[1] = 1
+                        res = list(set(res) | set(arr2[0]))
+            result.append(res)
+    return result
+
+
+
+def makeArr2(data) -> list:
     if (type(data) != list):
         data = json.loads(data)
     res = []
@@ -132,25 +129,25 @@ def flatList2(data) -> list:
             res.append(item)
     return res 
 
+def itIsUsed(items, arr) -> bool:
+    for elem in items:
+        if elem in arr:
+            return True
+    return False
+
 def getSforItem(value, Sab) -> list:
     for item in Sab:
         if value in item:
             return item
     return []
 
-def isUsed(S_item, used) -> bool:
-    for item in S_item:
-        if item in used:
-            return True
-    return False
+def func(s1, s2, isStr) -> list:
+    S_AB = S(s1, s2, isStr)
+    AL = makeArr(s1)
+    BL = makeArr(s2)
+    SL2 = makeArr2(S_AB)
 
-def f2(str1, str2, isStr) -> list:
-    Sab = S(str1, str2, isStr)
-    AL = flatList(str1)
-    BL = flatList(str2)
-    SL2 = flatList2(Sab)
-
-    used = []
+    usedArr = []
     n = len(AL)
     x = []
     for i in range(n):
@@ -162,116 +159,87 @@ def f2(str1, str2, isStr) -> list:
     resL = []
     for i in range(n):
         isAddA = False
-        if (AL[i][2] == 0) and (AL[i][0] in x) and (not isUsed([AL[i][0]], used)):
+        if (AL[i][2] == 0) and (AL[i][0] in x) and (not itIsUsed([AL[i][0]], usedArr)):
             resL.append(AL[i][0])
-            used.append(AL[i][0])
+            usedArr.append(AL[i][0])
             isAddA = True
 
         isAddB = False
-        if (BL[i][2] == 0) and (BL[i][0] in x) and (not isUsed([BL[i][0]], used)):
+        if (BL[i][2] == 0) and (BL[i][0] in x) and (not itIsUsed([BL[i][0]], usedArr)):
             resL.append(BL[i][0])
-            used.append(BL[i][0])
+            usedArr.append(BL[i][0])
             isAddB = True
 
-        if (not isUsed([AL[i][0]], used)) and (not isAddA):
-            S_item = getSforItem(AL[i][0], Sab)
+        if (not itIsUsed([AL[i][0]], usedArr)) and (not isAddA):
+            S_item = getSforItem(AL[i][0], S_AB)
             if (len(S_item) > 0):
                     for item in S_item:
-                        used.append(item)
+                        usedArr.append(item)
                     resL.append(S_item)
 
-        if (not isUsed([BL[i][0]], used)) and (not isAddB):
-            S_item = getSforItem(BL[i][0], Sab)
+        if (not itIsUsed([BL[i][0]], usedArr)) and (not isAddB):
+            S_item = getSforItem(BL[i][0], S_AB)
             if (len(S_item) > 0):
                     for item in S_item:
-                        used.append(item)
+                        usedArr.append(item)
                     resL.append(S_item)
 
         if (not isAddA) and (not isAddB):
             L = list(set(AL[i][3]) & set(BL[i][3]))
             L2 = []
             for item in L:
-                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not itIsUsed([item], usedArr)):
                     L2.append(item)
             if len(L2) == 1:
-                if not isUsed([L2[0]], used):
+                if not itIsUsed([L2[0]], usedArr):
                     resL.append(L2[0])
-                    used.append(L2[0])
+                    usedArr.append(L2[0])
             elif len(L2) > 1:
                 resL.append(L2)
                 for item in L2:
-                    used.append(item)
+                    usedArr.append(item)
         elif (not isAddA):
             L = AL[i][3]
             L2 = []
             for item in L:
-                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not itIsUsed([item], usedArr)):
                     L2.append(item)
             if len(L2) == 1:
-                if not isUsed([L2[0]], used):
+                if not itIsUsed([L2[0]], usedArr):
                     resL.append(L2[0])
-                    used.append(L2[0])
+                    usedArr.append(L2[0])
             elif len(L2) > 1:
                 resL.append(L2)
                 for item in L2:
-                    used.append(item)
+                    usedArr.append(item)
         elif (not isAddB):
             L = BL[i][3]
             L2 = []
             for item in L:
-                if (not (item in SL2)) and (not (item in x)) and (not isUsed([item], used)):
+                if (not (item in SL2)) and (not (item in x)) and (not itIsUsed([item], usedArr)):
                     L2.append(item)
             if len(L2) == 1:
-                if not isUsed([L2[0]], used):
+                if not itIsUsed([L2[0]], usedArr):
                     resL.append(L2[0])
-                    used.append(L2[0])
+                    usedArr.append(L2[0])
             elif len(L2) > 1:
                 resL.append(L2)
                 for item in L2:
-                    used.append(item)
+                    usedArr.append(item)
         
     return resL
 
-def task(str1, str2) -> list:
-    return f2(str1, str2, isStrResultType(str1, str2))
+def task(s1, s2) -> list:
+    return func(s1, s2, isStringType(s1, s2))
 
 def main():
-    #str1 = '["1", ["2","3"],"4", ["5", "6", "7"], "8", "9", "10"]'
-    #str2 = '[["1","2"], ["3","4","5"], "6", "7", "9", ["8","10"]]'
+    s1 = '[1,[2,3],4,[5,6,7],8,9,10]'
+    s2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
+    s3 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
 
-    #str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
-    #str2 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
-
-    #str1 = '["1",["2","3"],"4",["5","6","7"],"8","9","10"]'
-    #str2 = '["3",["1","4"],"2","6",["5","7","8"],["9","10"]]'
-
-    #str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
-    #str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
-
-
-
-    '''
-    str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
-    str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
-    str3 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
-
-    str1 = '["1",["2","3"],"4",["5","6","7"],"8","9","10"]'
-    str2 = '[["1","2"],["3","4","5"],"6","7","9",["8","10"]]'
-    str3 = '["3",["1","4"],"2","6",["5","7","8"],["9","10"]]'
-
-    str1 = '[[1], [2, 3, 4], [5, 6, 7], 8, 9, 10]' 
-    str2 = '[[1, 2, 3], [4, 5], 6, 7, 9, [8, 10]]' 
-    str3 = '[1, 4, 3, 2,6,[5,7,8],[9,10]]'         
-
-    '''
-
-    str1 = '[1,[2,3],4,[5,6,7],8,9,10]'
-    str2 = '[[1,2],[3,4,5],6,7,9,[8,10]]'
-    str3 = '[3,[1,4],2,6,[5,7,8],[9,10]]'
-
-    print(task(str1, str2))
-    #print(task(str2, str3))
-    #print(task(str1, str3))
+    print(task(s1, s2))
+    #print(task(s2, s3))
+    #print(task(s1, s3))
 
 
 if __name__ == "__main__":
--- task6/task.py
+++ task6/task.py
@@ -1,54 +1,55 @@
 import json
 import numpy as np
 
-def list_of_reviews_from_json(str, template):
-  reviews = json.loads(str)
-  reviews_list = [0 for _ in range(len(template))]
+def arrFromJson(string, t):
+  revs = json.loads(string)
+  arrRevs = [0 for _ in range(len(t))]
 
-  for i in range(len(reviews)):
-    if type(reviews[i]) is list:
-      for el in reviews[i]:
-        reviews_list[template[el]] = i + 1
+  for k in range(len(revs)):
+    if type(revs[k]) is list:
+      for item in revs[k]:
+        arrRevs[t[item]] = k + 1
     else:
-      reviews_list[template[reviews[i]]] = i + 1
+      arrRevs[t[revs[k]]] = k + 1
   
-  return reviews_list
+  return arrRevs
 
 def task(*args):
-  experts_count = len(args)
-  template = dict()
-  reviews_count = 0
+  exps_number = len(args)
+  dictTemp = dict()
+  revs_number = 0
 
-  for el in json.loads(args[0]):
-    if type(el) is list:
-      for elem in el:
-        template[elem] = reviews_count
-        reviews_count += 1
+  for item in json.loads(args[0]):
+    if type(item) is list:
+      for k in item:
+        dictTemp[k] = revs_number
+        revs_number += 1
     else:
-      template[el] = reviews_count
-      reviews_count += 1
+      dictTemp[item] = revs_number
+      revs_number += 1
       
-  matrix = []
-  for reviews_str in args:
-    matrix.append(list_of_reviews_from_json(reviews_str, template))
+  M = []
+  for revs_string in args:
+    M.append(arrFromJson(revs_string, dictTemp))
 
-  x = matrix
-  matrix = []
-  for i in range(reviews_count):
-    sum = 0
-    for j in range(experts_count):
-      sum += x[j][i]
-    matrix.append(sum)
-  matrix = np.matrix(matrix)
+  x = M
+  M = []
+  for k in range(revs_number):
+    amount = 0
+    for h in range(exps_number):
+      amount += x[h][k]
+    M.append(amount)
+  M = np.matrix(M)
 
-  # дисперсия
-  D = np.var(matrix) * reviews_count / (reviews_count - 1)
-  D_max = experts_count ** 2 * (reviews_count ** 3 - reviews_count) / 12 / (reviews_count - 1)
+  twelve = 12
+  revs_number_1 = revs_number - 1
+  Disp = np.var(M) * revs_number / revs_number_1
+  Disp_MAX = exps_number ** 2 * (revs_number ** 3 - revs_number) / twelve / revs_number_1
 
-  return format(D / D_max, ".2f")
+  return format(Disp / Disp_MAX, ".2f")
 
-A = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
-B = '[["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]'
-C = '["3", ["1", "4"], "2", "6", ["5", "7", "8"], ["9", "10"]]'
+M1 = '["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"]'
+M2 = '[["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]]'
+M3 = '["3", ["1", "4"], "2", "6", ["5", "7", "8"], ["9", "10"]]'
 
-print(task(A, B, C))
+print(task(M1, M2, M3))
